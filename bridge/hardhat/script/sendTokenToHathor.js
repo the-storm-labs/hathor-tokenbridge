69811c59478cf112e0cf2259d15e6cced20f72e9
Roger <roger.ikari@gmail.com>
2023-12-06 15:12:38 -0300

    Script to send tokens to EVM

--- bridge/hardhat/script/sendTokenToHathor.js
+++ bridge/hardhat/script/sendTokenToHathor.js
@@ -1,3 +1,4 @@
+// How to run the script: npx hardhat run ./hardhat/script/isTokenAllowed.js --network rsktestnet
 const hre = require('hardhat');
 
 async function main() {
@@ -9,14 +10,22 @@ async function main() {
     const BridgeProxy = await deployments.get('BridgeProxy');
     const bridge = new web3.eth.Contract(Bridge.abi, BridgeProxy.address)
 
-    const methodReceiveTokensTo = bridge.methods
+    // const methodReceiveTokensTo = bridge.methods
+    //     .receiveTokensTo(31, 
+    //     '0x2bfe63ddb1de113f337ae454edf8bd1d4a19ee14', 
+    //     'wY5dNSAqCsmcimkgHig3CzZPjYRDyBWbjv', 
+    //     1000000000000000000n);
+
+    // const result = await methodReceiveTokensTo.call({ from: deployer.address });
+    // console.log('Send tokens result', result);  
+    
+    const receipt = await bridge.methodsS
         .receiveTokensTo(31, 
         '0x2bfe63ddb1de113f337ae454edf8bd1d4a19ee14', 
         'wY5dNSAqCsmcimkgHig3CzZPjYRDyBWbjv', 
-        10000000000000000000n);
+        1000000000000000000n).send({ from: deployer, gasLimit: 3000000 });
 
-    const result = await methodReceiveTokensTo.call({ from: deployer.address });
-    console.log('Send tokens result', result);    
+    console.log("Tokens sent, txHash: ", receipt.transactionHash);
 }
 
 main()
