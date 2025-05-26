c84874dadb9d19076666aef2bbe46ba33e69ee54
Roger <roger.ikari@gmail.com>
2025-05-26 18:24:40 -0300

    parameterize HathorFederationFactory

--- federator/src/contracts/HathorFederationFactory.ts
+++ federator/src/contracts/HathorFederationFactory.ts
@@ -2,15 +2,18 @@ import abi from './HathorFederation.json';
 import { ContractFactory } from './ContractFactory';
 import { IHathorFederation } from './IHathorFederation';
 import { IHathorFederationV1 } from './IHathorFederationV1';
+import { ConfigChain } from '../lib/configChain';
 
 export class HathorFederationFactory extends ContractFactory {
   public hathorFederationContract: IHathorFederationV1;
 
-  createInstance(): IHathorFederation {
+  createInstance(configChain?: ConfigChain): IHathorFederation {
     if (!this.hathorFederationContract) {
-      const web3 = this.getWeb3(process.env.HATHOR_STATE_CONTRACT_HOST_URL);
-      const chainId = Number.parseInt(process.env.FEDERATION_CHAIN);
-      const contract = new web3.eth.Contract(abi, process.env.HATHOR_STATE_CONTRACT_ADDR);
+      const host = configChain?.host ?? process.env.HATHOR_STATE_CONTRACT_HOST_URL;
+      const chainId = configChain?.chainId ?? Number.parseInt(process.env.FEDERATION_CHAIN);
+      const contractAddress = configChain?.federation ?? process.env.HATHOR_STATE_CONTRACT_ADDR;
+      const web3 = this.getWeb3(host);
+      const contract = new web3.eth.Contract(abi, contractAddress);
       this.hathorFederationContract = new IHathorFederationV1(contract, chainId, web3);
     }
     return this.hathorFederationContract;
